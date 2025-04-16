3fd3106cba28c7b114ae25e2fb45ad7c775a4c70
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-04-16 11:33:08 +0200

    section signUp - Add:translation and use Navigate and uuidv4 and data  localstorage with user

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -4,37 +4,37 @@ import Banner from "./Banner";
 import Header from "./Header";
 import { Outlet } from "react-router";
 import Footer from "./Footer";
+import { ToastContainer } from "react-toastify";
 
 export default function PageLayout() {
   const { i18n } = useTranslation();
   const [local, setLocal] = useState(localStorage.getItem("i18nextLng") || "en");
   const [direction, setDirection] = useState(localStorage.getItem("pageDirection") || (local === "ar" ? "rtl" : "ltr"));
 
-  // تعيين الاتجاه عند التحميل الأولي
   useEffect(() => {
     document.documentElement.dir = direction;
   }, [direction]);
 
   function changeLanguage(newLang: string) {
     const newDirection = newLang === "ar" ? "rtl" : "ltr";
-    
+
     setLocal(newLang);
     setDirection(newDirection);
     i18n.changeLanguage(newLang);
     document.documentElement.dir = newDirection;
-    
+
     localStorage.setItem("i18nextLng", newLang);
     localStorage.setItem("pageDirection", newDirection);
   }
   return (
     <>
+      <ToastContainer />
       <div dir={direction}>
         <Banner local={local} changeLanguage={changeLanguage} />
         <Header local={local} />
         <Outlet />
-        <Footer local={local}/>
+        <Footer local={local} />
       </div>
     </>
   );
 }
-
--- src/components/pages/auth/SignUp.tsx
+++ src/components/pages/auth/SignUp.tsx
@@ -1,8 +1,57 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FcGoogle } from "react-icons/fc";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { v4 as uuidv4 } from "uuid";
 
+interface User {
+  id: string;
+  name: string;
+  emailOrPhoneNumber: string;
+  password: string;
+
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  address?: string;
+  orders?: [];
+  reviews?: [];
+  collection?: [];
+}
 export default function SignUp() {
+  const [formInput, setFormInput] = useState<User>({
+    id:uuidv4(),
+
+    name: "",
+    emailOrPhoneNumber: "",
+    password: "",
+
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    orders: [],
+    reviews: [],
+    collection: [],
+  });
+
+  const navigate = useNavigate();
+
+  const [users, setUsers] = useState<User[]>(JSON.parse(localStorage.getItem("Data For User") || "[]"));
+
+  function signup() {
+    const newUser = [...users, formInput];
+
+    setUsers(newUser);
+
+    localStorage.setItem("Data For User", JSON.stringify(newUser));
+
+    toast.success(t("Account registration successful"));
+
+    setFormInput({ ...formInput, name: "", emailOrPhoneNumber: "", password: "" });
+    navigate("/SignIn");
+  }
   const { t } = useTranslation();
   return (
     <>
@@ -11,17 +60,46 @@ export default function SignUp() {
           <img className="w-[600px] h-[550px] hidden lg:block" src="/images/beatsnoop.png" alt="" />
         </div>
         <div className="w-[600px] h-[550px] flex justify-center items-center">
-          <form className=" w-[70%]">
+          <form
+            className=" w-[70%]"
+            onSubmit={(e) => {
+              e.preventDefault();
+              signup();
+            }}
+          >
             <span className="text-[36px] block">{t("Create an account")}</span>
             <span className="text-[12px] block py-3">{t("Enter your details below")} </span>
             <label className="">
-              <input className=" border-b-2 border-[#808080] my-2 p-2 pl-0 outline-0 block w-[100%]" type="text" placeholder={t("Name")} />
+              <input
+                value={formInput.name}
+                onChange={(e) => setFormInput({ ...formInput, name: e.target.value })}
+                className=" border-b-2 border-[#808080] my-2 p-2 px-[5px] outline-0 block w-[100%]"
+                type="text"
+                minLength={1}
+                required
+                placeholder={t("Name")}
+              />
             </label>
             <label>
-              <input className=" border-b-2 border-[#808080] my-2 p-2 pl-0 outline-0 block w-[100%]" type="email" placeholder={t("Email or Phone Number")} />
+              <input
+                value={formInput.emailOrPhoneNumber}
+                onChange={(e) => setFormInput({ ...formInput, emailOrPhoneNumber: e.target.value })}
+                className=" border-b-2 border-[#808080] my-2 p-2 px-[5px] outline-0 block w-[100%]"
+                type="email"
+                required
+                placeholder={t("Email or Phone Number")}
+              />
             </label>
             <label>
-              <input className="  border-b-2 border-[#808080] my-2 mb-6 p-2 pl-0 outline-0 block w-[100%]" type="password" placeholder={t("Password")} />
+              <input
+                value={formInput.password}
+                onChange={(e) => setFormInput({ ...formInput, password: e.target.value })}
+                className="  border-b-2 border-[#808080] my-2 mb-6 p-2 px-[5px] outline-0 block w-[100%]"
+                type="password"
+                minLength={8}
+                required
+                placeholder={t("Password")}
+              />
             </label>
             <div>
               <button className="text-center bg-[#db4444] w-[100%] text-white p-2 rounded cursor-pointer">{t("Create Account")} </button>

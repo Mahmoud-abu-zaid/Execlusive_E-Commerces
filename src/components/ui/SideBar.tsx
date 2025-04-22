28e8a7dfde810c541af732493a014ae3db24d27a
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-04-22 23:42:38 +0200

    Edit:border Section Side bar

--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router";
 
 export default function SideBar() {
+  const  borderDirection=localStorage.getItem("pageDirection")
   const { t } = useTranslation();
   const sideBarLinks = [
     { name: t("Woman’s Fashion"), path: "/" },
@@ -16,8 +17,8 @@ export default function SideBar() {
   ];
   return (
     <>
-      <div className={`border-[#d0d0d0] w-fit  border-r-2`}>
-        <ul className="py-[30px] px-20 w-[320px]">
+      <div className={`border-[#d0d0d0] w-fit  ${ borderDirection === "rtl" ? " border-l-2" : " border-r-2"}`}>
+        <ul className="py-[30px] px-10 w-[270px]">
           {sideBarLinks.map((link, index) => (
             <li className="p-2" key={index}>
               <Link to={link.path}>{link.name}</Link>

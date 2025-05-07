7901d97a09cbf85abea0bc011540d04436775226
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-05-07 20:04:58 +0300

    Add:Component Managers && Support for directions

--- src/components/pages/Managers.tsx
+++ src/components/pages/Managers.tsx
@@ -1,43 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoInstagram } from "react-icons/io5";
 import { TiSocialLinkedin } from "react-icons/ti";
 
 export default function Managers() {
   const managers = [
-    { imgManag: "/images/tom_cruise.png", nameManag: "Tom Cruise", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
-    { imgManag: "/images/will_Smith.png", nameManag: "Will Smith", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
+    { imgManag: "/public/images/tom_cruise.png", nameManag: "Tom Cruise", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
+    { imgManag: "/public/images/will_Smith.png", nameManag: "Will Smith", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
     {
-      imgManag: "/images/snapedit_1746612194757.png",
+      imgManag: "/public/images/snapedit_1746612194757.png",
       nameManag: "Mahmoud Abu Zaid",
       jop: "Front End Developer",
       media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> },
     },
-    { imgManag: "/images/emma_watson.png", nameManag: "Emma Watson", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
-    { imgManag: "/images/tom_cruise.png", nameManag: "Tom Cruise", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
+    { imgManag: "/public/images/emma_watson.png", nameManag: "Emma Watson", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
+    { imgManag: "/public/images/tom_cruise.png", nameManag: "Tom Cruise", jop: "Founder & Chairman", media: { iconst: <IoLogoInstagram />, iconI: <TiSocialLinkedin />, iconL: <FaXTwitter /> } },
   ];
 
-  const [currentSlide, setCurrentSlide] = useState(1);
+  const [currentSlide, setCurrentSlide] = useState(2);
+  const [cardsToShow, setCardsToShow] = useState(3);
+  const dir = localStorage.getItem("pageDirection");
 
-  const cardsToShow = 3;
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setCardsToShow(1);
+      } else if (window.innerWidth < 992) {
+        setCardsToShow(2);
+      } else {
+        setCardsToShow(3);
+      }
+      setCurrentSlide(1);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const goToSlide = (index: number) => {
     const maxIndex = Math.max(0, managers.length - cardsToShow);
-
     setCurrentSlide(Math.min(index, maxIndex));
   };
 
   return (
-    <div className="relative py-10">
+    <div className="relative">
       <div className="flex justify-center overflow-hidden relative">
-        <div className="flex transition-transform duration-300" style={{ transform: `translateX(-${currentSlide * (100 / cardsToShow)}%)` }}>
+        <div
+          className="flex transition-transform duration-300"
+          style={{
+            transform: dir === "rtl" ? `translateX(${currentSlide * (100 / cardsToShow)}%)` : `translateX(-${currentSlide * (100 / cardsToShow)}%)`,
+            width: `${managers.length * (100 / cardsToShow)}%`,
+          }}
+        >
           {managers.map((manager, index) => (
-            <div key={index} className="flex-shrink-0 w-full px-3" style={{ width: `${100 / cardsToShow}%` }}>
+            <div key={index} className="flex-shrink-0 px-3" style={{ width: `${100 / cardsToShow}%` }}>
               <div className="flex flex-col justify-center">
-                <div className=" h-[430px] flex justify-center items-end bg-gray-200">
-                  <img className="w-fit h-[387px]" src={manager.imgManag} alt={manager.nameManag} />
+                <div className="max-w-full h-[430px] flex justify-center items-end bg-gray-200">
+                  <img className="w-[280px] sm:w-[380px] h-[370px] " src={manager.imgManag} alt={manager.nameManag} />
                 </div>
-                <div className="mx-3">
+                <div className="mx-3 flex flex-col lg:items-start items-center">
                   <p className="py-2 text-3xl">{manager.nameManag}</p>
                   <p>{manager.jop}</p>
                   <div className="flex gap-2 py-2">
@@ -53,7 +77,7 @@ export default function Managers() {
       </div>
 
       <div className="flex justify-center gap-3 my-3">
-        {Array.from({ length: managers.length - cardsToShow + 1 }).map((_, index) => {
+        {Array.from({ length: cardsToShow === 1 ? managers.length : managers.length - cardsToShow + 1 }).map((_, index) => {
           return (
             <li
               key={index}
